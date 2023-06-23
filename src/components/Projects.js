60eaffe2b5ed5cf79c035ee52f2c4ceb44296a07
Tahaberk SOYSAL <137247868+tsoysal@users.noreply.github.com>
2023-06-24 01:16:15 +0300

    Update Projects links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,42 +5,42 @@ function Gallery() {
   return (
     <div className="grid-container">
       <div className="project">
-        <a href='https://github.com/berksysl/earthquake-app' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/earthquake-app' target='_blank' rel='noreferrer'>
           <div className='imageHolder project1'></div>
           <span>React JS</span>
           <h3>Turkey Last Earthquakes</h3>
         </a>
       </div>
       <div className="project">
-        <a href='https://github.com/berksysl/pomodoro-timer' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/pomodoro-timer' target='_blank' rel='noreferrer'>
           <div className='imageHolder project2'></div>
           <span>html-css-javascript</span>
           <h3>Pomodoro Timer</h3>
         </a>
       </div>
       <div className="project">
-        <a href='https://github.com/berksysl/hostel-web-site' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/hostel-web-site' target='_blank' rel='noreferrer'>
           <div className='imageHolder project3'></div>
           <span>Html-Sass</span>
           <h3>Hotel Web Site</h3>
         </a>
       </div>
       <div className="project">
-        <a href='https://github.com/berksysl/spotify-clone' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/spotify-clone' target='_blank' rel='noreferrer'>
           <div className='imageHolder project4'></div>
           <span>html-css-javascript</span>
           <h3>Spotify Clone</h3>
         </a>
       </div>
       <div className="project">
-        <a href='https://github.com/berksysl/Team_page' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/Team_page' target='_blank' rel='noreferrer'>
           <div className='imageHolder project5'></div>
           <span>html-css</span>
           <h3>Team Page</h3>
         </a>
       </div>
       <div className="project">
-        <a href='https://github.com/berksysl/peracityhotel' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/tsoysal/hotel-peracity-website' target='_blank' rel='noreferrer'>
           <div className='imageHolder project6'></div>
           <span>html-Sass</span>
           <h3>Hotel Web Site</h3>
